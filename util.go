deb2b471c26e8d500124b7d847f544825b44b04d
Siren <siren@kernal.eu>
2023-06-12 17:20:12 +0300

    Typo fixes

--- util.go
+++ util.go
@@ -3,13 +3,13 @@ package proto
 import "encoding/json"
 
 func GetScanData(r json.RawMessage) (*EventScanData, error) {
-	var e proto.EventScanData
+	var e EventScanData
 	err := json.Unmarshal(r, &e)
 	return &e, err
 }
 
 func GetMoneyinData(r json.RawMessage) (*EventMoneyinData, error) {
-	var e proto.EventScanData
+	var e EventMoneyinData
 	err := json.Unmarshal(r, &e)
 	return &e, err
 }
